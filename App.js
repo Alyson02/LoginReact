fff64407aa118fcdafaebb45a1012ccda529d4cb
Alyson <alyson.subrim2002@gmail.com>
2022-05-10 19:29:00 -0300

    Configurando ambiente

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {Button, Input, Text} from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+var email, password;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text h1>ENTRE COM O LOGIN!!!</Text>
+      <Input 
+      placeholder="Email"
+      leftIcon={{type:'font-awesome', name:'envelope'}}
+      onChangeText={value=>email = value}
+      KeyboardType="email-address"/>
+
+      <Input 
+      placeholder="Senha"
+      leftIcon={{type:'font-awesome', name:'lock'}}
+      onChangeText={value=>password = value}
+      secureTextEntry="true"/>  
+
+      <Button
+       icon={
+        <Icon
+        name="check"
+        size={15}
+        color="white"
+      />
+      }
+      
+      title="Entrar"
+      onPress={()=>entrar()}
+       />
     </View>
   );
 }
